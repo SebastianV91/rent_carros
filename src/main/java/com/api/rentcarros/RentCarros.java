1df8b481ef9c1b0e2a4519fdd8d486c77c190a83
SebastianV91 <severgara91@hotmail.com>
2024-01-19 10:42:33 -0500

    first commit

---
+++ src/main/java/com/api/rentcarros/RentCarros.java
@@ -0,0 +1,13 @@
+package com.api.rentcarros;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RentCarros {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RentCarros.class, args);
+	}
+
+}
---
+++ src/test/java/com/api/rentcarros/RentCarrosTests.java
@@ -0,0 +1,13 @@
+package com.api.rentcarros;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RentCarrosTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
